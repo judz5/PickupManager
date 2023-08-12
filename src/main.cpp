de382b30873c5117ff3c9db0072d2a6955dac8b9
judz5 <judz1105@gmail.com>
2023-08-12 12:50:05 -0500

    menu v0.1

--- src/main.cpp
+++ src/main.cpp
@@ -6,7 +6,7 @@ using namespace std;
 
 
 void makeFile(const Order& _order){
-    ofstream file("data/orders/" + _order.getName() + ".csv");
+    ofstream file("data/" + _order.getName() + ".csv");
     file << _order.getName() << endl;
     file << _order.getLocation() << endl;
     file << _order.getStorageDate() << endl;
@@ -14,7 +14,7 @@ void makeFile(const Order& _order){
     file.close();
 }
 
-void menu(){
+int menu(){
     cout << "Welcome to PickupManager" << endl;
     cout << "---------------------------------------\n\n";
     cout << "1. Add New Pickup\n";
@@ -22,9 +22,28 @@ void menu(){
     cout << "3. Edit Pickup\n";
     cout << "4. Exit\n";
 
-    int selection;
-    cout << "Enter Selection: ";
-    cin >> selection;
+    int select;
+    cout << "\nEnter Selection: ";
+    cin >> select;
+    return select;
+}
+
+void newOrder(){
+    string name; string location; string pickupDate;
+
+    cin.ignore (std::numeric_limits<std::streamsize>::max(), '\n'); 
+    cout << "\nEnter order name: ";
+    getline(cin, name);
+    cout << "Enter order location: ";
+    cin >> location;
+    cout << "Enter pickup Date (MM/DD/YYYY): ";
+    cin >> pickupDate;
+
+    Order order(name, location, pickupDate);
+
+    cout << "\nNew Order Made -----@----- " << order.getStorageDate() << endl;
+    makeFile(order);
+    cout << "Order File Created.";
 }
 
 // Main interface and function running from here
@@ -56,6 +75,18 @@ int main()
 
     */
 
-   menu();
+    int selection = menu();
+    if(selection == 1){
+        newOrder(); 
+    }else if(selection == 2){
+        // search
+    }else if(selection == 3){
+        // edit
+    }else if(selection == 4){
+        exit(0);
+    }else{
+        cout << "Invalid Input";
+    }
+
 
 }
